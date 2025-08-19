fda443ae66750ebab65b3fb70fcf2b33de54f79d
João Ramajo <joaoramajo744@gmail.com>
2025-08-19 17:32:15 -0300

    feat: implementa `Request` para abstrair requisições

---
+++ application/controllers/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+use app\core\Request;
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class UserController extends CI_Controller
+{
+    public function index()
+    {
+
+    }
+}
---
+++ application/core/Request.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace app\core;
+
+class Request
+{
+    protected $data;
+    protected $params;
+    protected $method;
+
+    public function __construct($method = null, $data = null, $params = null)
+    {
+        if(is_null($method)){
+            $method = $_SERVER['REQUEST_METHOD'];
+        }
+
+        if(is_null($data)){
+            $data = $_POST;
+        }
+
+        if(is_null($params)){
+            $params = $_GET;
+        }
+
+        $this->data = $data;
+        $this->method = $method;
+        $this->params = $params;
+    }
+
+    public function input($key)
+    {
+        return $this->data[$key];
+    }
+}
