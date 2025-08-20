af2e43bcc8895699b93c35091f3f44e419c4059f
João Ramajo <joaoramajo744@gmail.com>
2025-08-20 01:45:05 -0300

    chore: atualiza estrutra da tabela `articles` para não aceitar valores nulos

--- application/database/tables/articles.sql
+++ application/database/tables/articles.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS articles (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    user_id INT,
-    title VARCHAR(50),
-    content TEXT,
+    user_id INT NOT NULL,
+    title VARCHAR(50) NOT NULL,
+    content TEXT NOT NULL,
     CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id)
 );
